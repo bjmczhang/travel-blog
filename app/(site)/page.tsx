dc7b01949cb9af83d4315086f5240863a9f6beed
bjmczhang <bjmczhang@gmail.com>
2024-07-21 00:33:08 +1000

    nav and hero complete

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,11 @@
+import Hero from "@/components/shared/Hero";
+
 const Home = () => {
-  return <div className="h-[2000px]">Home</div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
--- components/ui/Tag.tsx
+++ components/ui/Tag.tsx
@@ -1,9 +1,15 @@
 interface tagProps {
-  text: string;
+  text: string[];
 }
 
 const Tag = ({ text }: tagProps) => {
-  return <div>Tag</div>;
+  return (
+    <>
+      <span className="uppercase bg-primary py-1 px-3 text-white inline-block rounded-md text-sm self-center my-2">
+        {text}
+      </span>
+    </>
+  );
 };
 
 export default Tag;
